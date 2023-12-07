2a5e6a121cc1721556e48ec677ac2d34e3202776
Fric12 <134865158+Fric12@users.noreply.github.com>
2023-12-08 00:16:19 +0200

    Update Semaphore.swift

--- Semaphore.swift
+++ Semaphore.swift
@@ -7,39 +7,32 @@ var accountBalance = 1000
 
 func withdraw(amount: Int) {
     concurrentQueue.async {
-        // Очікуємо, поки семафор стане доступним
         balanceAccessSemaphore.wait()
 
         if accountBalance > amount {
-            // Симулюємо затримку для демонстрації
             Thread.sleep(forTimeInterval: 1)
             accountBalance -= amount
-            print("Успішне зняття коштів. Залишок: \(accountBalance)")
+            print("Successful withdrawal. Remaining balance: \(accountBalance)")
         } else {
-            print("Недостатньо коштів")
+            print("Insufficient funds")
         }
 
-        // Позначаємо, що критична секція завершилася
         balanceAccessSemaphore.signal()
     }
 }
 
 func refillBalance(amount: Int) {
     concurrentQueue.async {
-        // Очікуємо, поки семафор стане доступним
         balanceAccessSemaphore.wait()
 
-        // Симулюємо затримку для демонстрації
         Thread.sleep(forTimeInterval: 1)
         accountBalance += amount
-        print("Успішне поповнення. Залишок: \(accountBalance)")
+        print("Successful refill. Remaining balance: \(accountBalance)")
 
-        // Позначаємо, що критична секція завершилася
         balanceAccessSemaphore.signal()
     }
 }
 
-// Симулюємо кілька виведень коштів, які відбуваються паралельно
 for _ in 1...5 {
     withdraw(amount: 150)
     refillBalance(amount: 200)
