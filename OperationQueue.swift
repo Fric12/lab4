5caea9a2c1223682e596f893231493dd60433414
Fric12 <134865158+Fric12@users.noreply.github.com>
2023-12-15 00:07:16 +0200

    Update OperationQueue.swift

--- OperationQueue.swift
+++ OperationQueue.swift
@@ -1,37 +1,35 @@
 import Foundation
 
 let concurrentQueue = OperationQueue()
-let balanceAccessQueue = OperationQueue()
-
 var accountBalance = 1000
 
 func withdraw(amount: Int) {
-    balanceAccessQueue.addOperation {
-        concurrentQueue.addOperation {
-            if accountBalance > amount {
-                // Simulate delay for demonstration purposes
-                Thread.sleep(forTimeInterval: 1)
-                accountBalance -= amount
-                print("Successful withdrawal. Remaining balance: \(accountBalance)")
-            } else {
-                print("Insufficient funds")
-            }
-        }
+  concurrentQueue.addOperation {
+    balanceQueue.sync {
+      if accountBalance >= amount { // Simulate delay for demonstration purposes
+        //Thread.sleep(forTimeInterval: 1)
+        accountBalance -= amount
+        print("Withdrawal successful. Remaining balance: \(accountBalance)")
+      } else {
+        print("Insufficient funds")
+      }
     }
+  }
 }
 
 func refillBalance(amount: Int) {
-    balanceAccessQueue.addOperation {
-        concurrentQueue.addOperation {
-            // Simulate delay for demonstration purposes
-            Thread.sleep(forTimeInterval: 1)
-            accountBalance += amount
-            print("Successful refill. Remaining balance: \(accountBalance)")
-        }
+  concurrentQueue.addOperation {
+    balanceQueue.sync {
+      // Simulate delay for demonstration purposes
+      //Thread.sleep(forTimeInterval: 1)
+      accountBalance += amount
+      print("Refill successful. Remaining balance: \(accountBalance)")
     }
+  }
 }
 
+// Simulate multiple withdrawals happening concurrentl
 for _ in 1...5 {
-    withdraw(amount: 150)
-    refillBalance(amount: 200)
+  withdraw(amount: 150)
+  refillBalance(amount: 200)
 }
