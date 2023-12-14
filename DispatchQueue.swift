76b54681c0b3ffaa7bc1034eb60ee6492ab5165b
Fric12 <134865158+Fric12@users.noreply.github.com>
2023-12-15 00:06:44 +0200

    Update DispatchQueue.swift

--- DispatchQueue.swift
+++ DispatchQueue.swift
@@ -1,38 +1,30 @@
 import Foundation
 
 let concurrentQueue = DispatchQueue(label: "com.example.concurrentQueue", attributes: .concurrent)
-let balanceAccessQueue = DispatchQueue(label: "com.example.balanceAccessQueue", attributes: .concurrent)
-
 var accountBalance = 1000
+let dispatchGroup = DispatchGroup()
 
 func withdraw(amount: Int) {
-    balanceAccessQueue.async {
-        concurrentQueue.async {
-            if accountBalance > amount {
-                // Simulate delay for demonstration purposes
-                Thread.sleep(forTimeInterval: 1)
-                accountBalance -= amount
-                print("Successful withdrawal. Remaining balance: \(accountBalance)")
-            } else {
-                print("Insufficient funds")
-            }
+    concurrentQueue.async(group: dgroup) {
+        if accountBalance >= amount {
+            //Thread.sleep(forTimeInterval: 1)
+            accountBalance -= amount
+            print("Withdrawal successful. Remaining balance: \(accountBalance)")
+        } else {
+            print("Insufficient funds")
         }
     }
 }
 
 func refillBalance(amount: Int) {
-    balanceAccessQueue.async {
-        concurrentQueue.async {
-            // Simulate delay for demonstration purposes
-            Thread.sleep(forTimeInterval: 1)
-            accountBalance += amount
-            print("Successful refill. Remaining balance: \(accountBalance)")
-        }
+    concurrentQueue.async(group: dgroup) {
+        //Thread.sleep(forTimeInterval: 1)
+        accountBalance += amount
+        print("Refill successful. Remaining balance: \(accountBalance)")
     }
 }
 
-
 for _ in 1...5 {
-    withdraw(amount: 150)
-    refillBalance(amount: 200)
+    withdraw(amount: 100)
+    refillBalance(amount: 100)
 }
